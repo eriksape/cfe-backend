878b5b43a04599092c169fcd54bbe08623b90a5a
Erik Sanchez <erik.sape@gmail.com>
2017-02-10 16:27:18 -0600

    adding cars middleware

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    /**
+     * Run the request filter.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+        $response->header('Access-Control-Allow-Origin', 'http://localhost:3000');
+        return $response;
+    }
+
+}
