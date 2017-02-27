82dd205e522f16ada43d8cb1d129e2ca176dcbe1
Erik Sanchez <erik.sape@gmail.com>
2017-02-27 08:19:54 -0600

    Agregando ultima captura

--- app/Models/Medidor.php
+++ app/Models/Medidor.php
@@ -8,6 +8,14 @@ class Medidor extends Model
 {
     protected $table = 'medidores';
 
+    protected $appends = ['ultima_captura'];
+
+    public function getUltimaCapturaAttribute()
+    {
+        $captura = Captura::whereMedidorId($this->id)->orderBy('created_at', 'desc')->first();
+        return is_null($captura->captura_final)?$captura->captura_inicial:$captura->captura_final;
+    }
+
     public function capturas()
     {
         return $this->hasMany(Captura::class, 'medidor_id');
